1d162008510a8a409d3dee9aa81cc520bc2e4a86
nobody2007-afk <harrydazan@icloud.com>
2026-08-06 10:35:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// const ctx1 = document.querySelector('#canva1').getContext('2d');
+// const ctx2 = document.querySelector('#canva2').getContext('2d');
+// ctx1.fillStyle = '#ffffff';
+// ctx1.font = "20px Times New Roman";
+// ctx1.fillText("Ecole Certifiée ISO", 900, 50);
